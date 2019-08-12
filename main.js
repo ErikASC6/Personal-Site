0ccb75168826ffe82327387a862c663b28c49ac6
ErikASC6 <52749414+ErikASC6@users.noreply.github.com>
2019-08-12 11:14:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const introParagraph = document.createElement("p");
+introParagraph.innerText = "Click";
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
